7549fdf66fa47c1624ba358bc29ac4952b3d311d
yoyo930021 <yoyo930021@gmail.com>
2018-03-18 19:12:24 +0800

    Add persisted

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
-    maxId: 4,
+    maxId: 8,
     filter: false,
     board: {
       list: [
